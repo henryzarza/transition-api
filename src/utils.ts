8f08def9dc08c42f360a53a84188c5c16045f009
Henry Zarza <henryacz7@gmail.com>
2023-08-14 18:08:37 +0200

    Image transform utils

---
+++ src/utils.ts
@@ -0,0 +1,21 @@
+export async function getData(method: string, limit = '50') : Promise<any> {
+  const res = await fetch(`${import.meta.env.API_URL}&method=${method}&api_key=${import.meta.env.API_KEY}&limit=${limit}`);
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+export function checkImage(
+  image: { '#text': string }[],
+  positionToCheck: number,
+  defaultImgConfig = '200'
+  ) : string {
+  const imgUrl = ((image.length > positionToCheck + 1) && image[positionToCheck] && image[positionToCheck]['#text']) || '';
+
+  // this URL is a generic image provided by Last.fm APi
+  // I don't like that default image that's the reason I'm changing it :)
+  return imgUrl.includes('https://lastfm.freetls.fastly.net/i/u/') ? `https://picsum.photos/${defaultImgConfig}` : imgUrl;
+}
