d65699f1561fc80e1cdb4f4b7003d342bc06f65c
Henry Zarza <henryacz7@gmail.com>
2023-08-16 13:42:34 +0200

    Favorite artist page and detail

---
+++ src/constants.ts
@@ -0,0 +1,62 @@
+export const FAVORITE_ARTISTS = [
+  {
+    id: 1,
+    name: 'Dua Lipa',
+    imageSrc: 'https://res.cloudinary.com/dcqu0udnd/image/upload/v1692176147/transition-api/dua-lipa_nlhzem.jpg'
+  },
+  {
+    id: 2,
+    name: 'Lady Gaga',
+    imageSrc: 'https://res.cloudinary.com/dcqu0udnd/image/upload/v1692176146/transition-api/gaga_v1q6e4.jpg'
+  },
+  {
+    id: 3,
+    name: 'Britney Spears',
+    imageSrc: 'https://res.cloudinary.com/dcqu0udnd/image/upload/v1692176147/transition-api/britney_ibo8eg.png'
+  },
+  {
+    id: 4,
+    name: 'Rüfüs Du Sol',
+    imageSrc: 'https://res.cloudinary.com/dcqu0udnd/image/upload/v1692176147/transition-api/rufus_uu6nxn.jpg'
+  },
+  {
+    id: 5,
+    name: 'Claptone',
+    imageSrc: 'https://res.cloudinary.com/dcqu0udnd/image/upload/v1692176146/transition-api/claptone_jpzxtb.jpg'
+  },
+  {
+    id: 6,
+    name: 'Kygo',
+    imageSrc: 'https://res.cloudinary.com/dcqu0udnd/image/upload/v1692176146/transition-api/kygo_tqafeg.jpg'
+  },
+  {
+    id: 7,
+    name: 'Tiësto',
+    imageSrc: 'https://res.cloudinary.com/dcqu0udnd/image/upload/v1692176147/transition-api/tiesto_tqosr8.jpg'
+  },
+  {
+    id: 8,
+    name: 'Rihanna',
+    imageSrc: 'https://res.cloudinary.com/dcqu0udnd/image/upload/v1692176146/transition-api/rihanna_zxnyhu.webp'
+  },
+  {
+    id: 9,
+    name: 'Armin Van Buuren',
+    imageSrc: 'https://res.cloudinary.com/dcqu0udnd/image/upload/v1692176146/transition-api/armin_x6oz3a.jpg'
+  },
+  {
+    id: 10,
+    name: 'David Guetta',
+    imageSrc: 'https://res.cloudinary.com/dcqu0udnd/image/upload/v1692176146/transition-api/guetta_gj3t5m.jpg'
+  },
+  {
+    id: 11,
+    name: 'Major Lazer',
+    imageSrc: 'https://res.cloudinary.com/dcqu0udnd/image/upload/v1692176147/transition-api/major_yo4lnn.jpg'
+  },
+  {
+    id: 12,
+    name: 'GIMS',
+    imageSrc: 'https://res.cloudinary.com/dcqu0udnd/image/upload/v1692176146/transition-api/gims_hwa6ku.jpg'
+  }
+];
